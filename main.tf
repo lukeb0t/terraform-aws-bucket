567daf18d0a9e5149defd0c79f2a796b2a0da431
Luke Mccleary <32186417+lukeb0t@users.noreply.github.com>
2021-04-26 12:59:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,46 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "argonet-5" {
-  cidr_block           = var.address_space
-  enable_dns_hostnames = true
-  tags = var.aws_tags
-}
-
-resource "aws_security_group" "argonet-5" {
-  name   = "${var.prefix}-security-group"
-  vpc_id = aws_vpc.argonet-5.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
+resource "aws_s3_bucket" "bucket1" {
+  bucket = var.bucketname
+  acl    = "private"
 
   tags = var.aws_tags
-
 }
-
