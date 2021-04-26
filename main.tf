4cc302390aa57e89aa578d2b60f170bbf41aa63b
Luke Mccleary <32186417+lukeb0t@users.noreply.github.com>
2021-04-26 13:21:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bawdw435asefg2675y4586luke" {
-  bucket = "${var.prefix}_${random_id.bucket_id.hex}"
+  bucket = "${var.prefix}-${random_id.bucket_id.hex}"
   acl    = "private"
   tags = var.aws_tags
 }
