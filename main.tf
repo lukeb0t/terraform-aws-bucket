eabe61c1106e21d90c66d9a8cb86ef623c5a1ea5
Luke Mccleary <32186417+lukeb0t@users.noreply.github.com>
2021-04-26 13:13:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "${var.prefix}_${random_id.server.hex}"
+  bucket = "${var.prefix}_${random_id.bucket_id.hex}"
   acl    = "private"
   tags = var.aws_tags
 }
